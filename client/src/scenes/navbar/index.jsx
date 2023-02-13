a6181965dc46a0174e352ac88eb41909128e6329
Diego Herrera <dagomessis@gmail.com>
2023-02-13 15:21:20 -0500

    Navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
         <Typography
           fontWeight="bold"
           fontSize="clamp(1rem, 2rem, 2.25rem)"
-          color="green"
+          color="primary"
           onClick={() => navigate("/home")}
           sx={{
             "&:hover": {
