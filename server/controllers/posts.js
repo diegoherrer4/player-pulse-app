496775db48830b1c5cd2f88722d9debc22365ea6
Diego Herrera <dagomessis@gmail.com>
2023-03-06 19:05:15 -0500

    Added console.logs to try and debug the app

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -41,6 +41,7 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (req, res) => {
   try {
     const post = await Post.find();
+    console.log(`Posts: ${post}`);
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
