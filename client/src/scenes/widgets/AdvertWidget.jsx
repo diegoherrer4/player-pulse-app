584059eca59f0485baf117092c128a7f7d362f0b
Diego Herrera <dagomessis@gmail.com>
2023-03-08 19:55:41 -0500

    csp advert

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="https://cors-anywhere.herokuapp.com/https://aws-s3-playerpulse-bucket.s3.amazonaws.com/ad-litter.jpg"
+        src="https://aws-s3-playerpulse-bucket.s3.amazonaws.com/ad-litter.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
